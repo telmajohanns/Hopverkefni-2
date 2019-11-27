5a196ce3c3f98474e375a3c2144b6a7bb6c5bdd3
BjarkiGr <bjg56@hi.is>
2019-11-27 14:04:35 +0000

    lecture js prufa

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/prefer-default-export */
 export function empty(element) {
   while (element.firstChild) {
     element.removeChild(element.firstChild);
---
+++ src/lib/lecture.js
@@ -0,0 +1,34 @@
+import { el } from './helpers';
+
+export default class Lecture {
+  constructor() {
+    this.container = document.querySelector('.lecture');
+    this.url = 'lectures.json';
+  }
+
+  async loadLecture(slug) {
+    const res = await fetch(this.url);
+    if (!res.ok) {
+      throw new Error('Gat ekki sótt fyrirlestra');
+    }
+    const data = await res.json();
+    const found = data.lectures.find(i => i.slug === slug);
+    if (!found) {
+      throw new Error('Fyrirlestur fannst ekki');
+    }
+    return found;
+  }
+}
+
+createHeader(data) {
+  const category = el('div', data.category);
+  category.classList.add('lecture__header--category');
+  const headingElement = el('h1', data.title);
+  headingElement.classList.add('lecture__header--title');
+
+  if (data.image) {
+    headingWrapper.style.backgroundImage = `url(${data.image})`;
+  }
+
+  return headingWrapper;
+}
