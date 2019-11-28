b3062d27bdd62d2e77d5f286cf77175270cd4ac3
marialisath <marialisa@simnet.is>
2019-11-28 19:59:58 +0000

    javascript

--- src/lib/list.js
+++ src/lib/list.js
@@ -1,44 +1,88 @@
 import { empty, el } from './helpers';
+import * as converter from './converter';
+import { loadSavedLectures } from './storage';
 
 export default class List {
   constructor() {
     this.container = document.querySelector('.list');
+    this.url = './lectures.json';
+
+    this.completedLectures = loadSavedLectures();
+
+    this.htmlButton = document.querySelector('.navbar__HTML');
+    this.htmlButton.addEventListener('click', this.catButtonHandler.bind(this));
+
+    this.cssButton = document.querySelector('.navbar__CSS');
+    this.cssButton.addEventListener('click', this.catButtonHandler.bind(this));
+
+    this.jsButton = document.querySelector('.navbar__JS');
+    this.jsButton.addEventListener('click', this.catButtonHandler.bind(this));
   }
 
-  load() {
-    empty(this.container);
-    const list = fetch('lectures.json')
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .then(json => {
-        // for each lecture, create lecture list item and append to right place.
-
-        console.log(json);
+  loadLectures() {
+    return fetch(this.url)
+    .then((res) => {
+      if (!res.ok){
+        throw new Error('Gat ekki sótt fyrirlestra');
       }
-    const lectures = list.lectures;
-  }
+      return res.json();
+    });
+      }
 
-  // create one lecture list item like in the útlit image.
-  createLectureListItem(title, category, thumbnail, slug){
-  // <div class="card html">
-  //   <a href="./fyrirlestur.html?slug=html-sagan"></a>
-  //   <img class="card__picture" src="./img/thumb1.jpg" />
-  //   <div class="card__content">
-  //     <div class="card__text">
-  //       <p>HTML</p>
-  //       <h2>Sagan</h2>
-  //     </div>
-  //   </div>
-  // </div>
+    renderData(data, x) {
+      if (typeof x !== 'undefined' && x.length > 0){
+        for (let i=0; i<x.length; i+= 1){
+          for(let item of data.lectures){
+            if (x[i] === item.category){
+              this.renderItem(item);
+            }
+          }
+        }
+      } else {
+        for(let item of data.lectures){
+          this.renderItem(item);
+        }
+      }
+    }
 
-  // búa til öll elements
+    renderItem(item){
+      const pictureContainer = converter.generatePictureContainer(item.category, item.slug);
 
-  // setja rétta klasa og src á mynd
+      const imageElement = converter.generateCardPicture(item.picture);
+      pictureContainer.appendChild(imageElement);
+      let titleElement;
 
-  // return element
+      if (this.completedLectures.includes(item.slug)){
+        titleElement = converter.generateCardContent(item.title, item.category, 'card__checked');
+      }else {
+        titleElement = converter.generateCardContent(item.title, item.category);
+      }
+      pictureContainer.appendChild(titleElement);
 
-  }
-}
+      this.container.appendChild(pictureContainer);
+      }
+
+      load(x){
+        empty(this.container);
+        this.loadLectures()
+        .then((data) => {
+          this.renderData(data,x);
+        });
+    }
+
+    refreshList(){
+      const selected = document.querySelectorAll('.navbar__selected');
+      const x = [];
+      for (let el of selected){
+        x.push(el.firstChild.nodeValue.toLowerCase());
+      }
+      this.load(x);
+    }
+  
+    xButtonHandler(e){
+      const {target } = e;
+      target.classList.toggle('navbar__selected');
+      this.refreshList();
+
+    }
+    }
---
+++ src/lib/storage.js
@@ -0,0 +1,35 @@
+const LOCALSTORAGEKEY = 'completed_lectures';
+
+export function loadSavedLectures(){
+    const lecturesJson = localStorage.getItem(LOCALSTORAGEKEY);
+    const lectures = JSON.parse(lecturesJson) || [];
+
+    return lectures;
+}
+
+function removeSavedLecture(slug) {
+    const lectures = loadSavedLectures();
+    const index = lectures.indexOf(slug);
+
+    if (index > -1){
+        lectures.splice(index, 1);
+    }
+    localStorage.setItem(LOCALSTORAGEKEY, JSON.stringify(lectures));
+    }
+
+
+    export function saveLecture(slug){
+        const lectures = loadSavedLectures();
+        if (!lectures.includes(slug)){
+            lectures.push(slug);
+            localStorage.setItem(LOCALSTORAGEKEY, JSON.stringify(lectures));
+        } else {
+            removeSavedLecture(slug);
+        }
+    }
+
+
+    export function lectureCompleted(slug){
+        const lectures = loadSavedLectures();
+        return lectures.includes(slug);
+    }
