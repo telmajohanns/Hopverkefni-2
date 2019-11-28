3f23466cb7b85aa1dc6a453f53ad4847fde47ff1
BjarkiGr <bjg56@hi.is>
2019-11-28 16:53:52 +0000

    lagar eslint villr

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,9 @@ module.exports = {
   rules: {
     'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
     'class-methods-use-this': 0,
+    'linebreak-style': 0,
+    'arrow-parens': 0,
+    'prefer-destructuring': 0,
   },
   plugins: ['import'],
 };
--- src/lib/list.js
+++ src/lib/list.js
@@ -1,21 +1,17 @@
 import { empty, el } from './helpers';
 
-
 export default class List {
   constructor() {
     this.container = document.querySelector('.list');
   }
 
-
-
   load() {
     empty(this.container);
-    const list = fetch ('lectures.json')
-    .then((response) => {
+    const list = fetch('lectures.json').then(response => {
       if (response.ok) {
         return response.json();
       }
-  });
-  const lectures = list.lectures;
+    });
+    const lectures = list.lectures;
   }
 }
